ace61a5071798e153c95ddefe2bd632d1ed5e94e
akankshajain18 <cs.akankshajain@gmail.com>
2020-11-05 13:10:29 +0530

    SLING-9535_-Added test case

--- src/test/java/org/apache/sling/resourceresolver/impl/mapping/MapEntriesTest.java
+++ src/test/java/org/apache/sling/resourceresolver/impl/mapping/MapEntriesTest.java
@@ -2085,4 +2085,48 @@ public class MapEntriesTest extends AbstractMappingMapEntriesTest {
         }
     }
 
-  }
+    @Test
+    public void test_simple_alias_support_PT() {
+        long sTime = System.currentTimeMillis();
+        System.out.println("[Start]test_simple_alias_support " + sTime);
+
+        Resource[] result_P = new Resource[testSize];
+        Resource[] parent_P = new Resource[testSize];
+        for(int i =0 ;i<testSize;i++){
+            result_P[i] = mock(Resource.class);
+            parent_P[i] = mock(Resource.class);
+            when(result_P[i].getPath()).thenReturn("/parent"+i+"/child" + i);
+            when(parent_P[i].getPath()).thenReturn("/parent"+i);
+            when(result_P[i].getParent()).thenReturn(parent_P[i]);
+            when(result_P[i].getName()).thenReturn("child"+i);
+            when(result_P[i].getValueMap()).thenReturn(buildValueMap(ResourceResolverImpl.PROP_ALIAS, "alias"));
+
+        }
+
+        when(resourceResolver.findResources(anyString(), eq("sql"))).thenAnswer(new Answer<Iterator<Resource>>() {
+
+            @Override
+            public Iterator<Resource> answer(InvocationOnMock invocation) throws Throwable {
+                if (invocation.getArguments()[0].toString().contains(ResourceResolverImpl.PROP_ALIAS)) {
+                    return Arrays.asList(result_P).iterator();
+                } else {
+                    return Collections.<Resource> emptySet().iterator();
+                }
+            }
+        });
+
+        mapEntries.doInit();
+        for(int i =1 ;i<testSize;i++) {
+            Map<String, String> aliasMap = mapEntries.getAliasMap("/parent" +i);
+            assertNotNull(aliasMap);
+            assertTrue(aliasMap.containsKey("alias"));
+            assertEquals("child"+i, aliasMap.get("alias"));
+        }
+        long eTime =  System.currentTimeMillis();
+        System.out.println("[End]test_simple_alias_support_PT " + eTime);
+        System.out.println("[Total Time Taken]test_simple_alias_support_PT " + (eTime - sTime));
+
+    }
+
+
+}
