674b2de9350b1e72430f0ceeb6e8e6f31ea7a44e
akankshajain18 <cs.akankshajain@gmail.com>
2020-12-16 14:27:19 +0530

    suppressing sonar

--- src/main/java/org/apache/sling/resourceresolver/impl/ResourceResolverFactoryActivator.java
+++ src/main/java/org/apache/sling/resourceresolver/impl/ResourceResolverFactoryActivator.java
@@ -123,7 +123,7 @@ public class ResourceResolverFactoryActivator {
     private volatile ResourceResolverFactoryConfig config = DEFAULT_CONFIG;
 
     /** Alias path whitelist */
-    @SuppressWarnings("PMD.AvoidUsingVolatile")
+    //NOSONAR
     private volatile List<String> aliasPathAllowList = Collections.unmodifiableList(new ArrayList<>());
 
     /** Vanity path whitelist */
