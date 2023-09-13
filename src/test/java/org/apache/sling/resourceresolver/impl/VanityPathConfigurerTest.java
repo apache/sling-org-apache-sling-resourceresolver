5cd101ba7257445cf2accea4d508e4c4ccba0584
Carsten Ziegeler <cziegeler@apache.org>
2023-09-13 14:39:43 +0200

    Issues/sling 11742 (#97)
    * [SLING-11742] Provide alternative equitable terminology for properties
    
    * Implement PR feedback.
    
    * Implement PR feedback.
    
    * Remove metatype info for old properties, move vanity path confiuguration to separate configurer
    
    * Update logging and configuration handling
    
    * Move vanity configuration to configurer class
    
    * Use separate annotation for old properties
    
    * Fix handling of missing config properties, improve logging
    
    * Switch to allow/deny list everywhere
    
    ---------
    
    Co-authored-by: cristian cioriia <cioriia@adobe.com>

---
+++ src/test/java/org/apache/sling/resourceresolver/impl/VanityPathConfigurerTest.java
@@ -0,0 +1,72 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.sling.resourceresolver.impl;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+public class VanityPathConfigurerTest {
+    private VanityPathConfigurer vanityPathConfigurer;
+
+    @Before
+    public void before() {
+        vanityPathConfigurer = new VanityPathConfigurer();
+    }
+
+    @Test
+    public void testConfigureVanityPathPrefixes_givenBothPathPrefixAndFallBack_thenPathPrefixIsUsed() {
+        String[] pathPrefixes = {"/some/path/"};
+        String[] pathPrefixesFallback = {"/some/fallback/path/"};
+
+        final List<String> result = vanityPathConfigurer.configureVanityPathPrefixes(pathPrefixes, pathPrefixesFallback,
+            "original", "fallback");
+        verifyResults(result, pathPrefixes);
+    }
+
+    @Test
+    public void testConfigureVanityPathPrefixes_givenOnlyPathPrefix_thenPathPrefixIsUsed() {
+        String[] pathPrefixes = {"/some/path/"};
+        String[] pathPrefixesFallback = null;
+
+        final List<String> result = vanityPathConfigurer.configureVanityPathPrefixes(pathPrefixes, pathPrefixesFallback,
+            "original", "fallback");
+        verifyResults(result, pathPrefixes);
+    }
+
+    @Test
+    public void testConfigureVanityPathPrefixes_givenOnlyPathPrefixFallback_thenPathPrefixFallbackIsUsed() {
+        String[] pathPrefixes = null;
+        String[] pathPrefixesFallback = {"/some/fallback/path/"};
+
+        final List<String> result = vanityPathConfigurer.configureVanityPathPrefixes(pathPrefixes, pathPrefixesFallback,
+            "original", "fallback");
+        verifyResults(result, pathPrefixesFallback);
+    }
+
+    private void verifyResults(List<String> actualResult, String[] expectedResults) {
+        assertEquals(expectedResults.length, actualResult.size());
+        for(int i=0; i<expectedResults.length; i++) {
+            assertEquals(expectedResults[i], actualResult.get(i));
+        }
+    }
+
+}
