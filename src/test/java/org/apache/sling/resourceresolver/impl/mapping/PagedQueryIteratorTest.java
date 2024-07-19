2913aa03bfb33b930967afe946ed6ba9c78fc931
Julian Reschke <reschke@apache.org>
2024-07-19 09:18:43 +0100

    SLING-12387: ResourceResolver: improve test coverage for PagedQueryIterator: simple query

--- src/test/java/org/apache/sling/resourceresolver/impl/mapping/PagedQueryIteratorTest.java
+++ src/test/java/org/apache/sling/resourceresolver/impl/mapping/PagedQueryIteratorTest.java
@@ -18,18 +18,26 @@
  */
 package org.apache.sling.resourceresolver.impl.mapping;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
 import org.apache.sling.api.resource.QuerySyntaxException;
 import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ValueMap;
 import org.apache.sling.api.resource.path.Path;
 import org.apache.sling.resourceresolver.impl.ResourceResolverMetrics;
 import org.junit.Before;
@@ -69,5 +77,29 @@ public class PagedQueryIteratorTest extends AbstractMappingMapEntriesTest {
     public void testMalformedQuery() {
         when(resourceResolver.findResources(eq("malformed"), eq("JCR-SQL2"))).thenThrow(new QuerySyntaxException("x", "y", "z"));
         mapEntries.new PagedQueryIterator("alias", PROPNAME, resourceResolver, "malformed", 2000);
+    }
+
+    @Test
+    public void testSimple() {
+        String[] expected = new String[] { "a", "b", "c" };
+        Collection<Resource> expectedResources = toResourceList(expected);
+        when(resourceResolver.findResources(eq("simple"), eq("JCR-SQL2"))).thenReturn(expectedResources.iterator());
+        Iterator<Resource> it = mapEntries.new PagedQueryIterator("alias", PROPNAME, resourceResolver, "simple", 2000);
+        for (String key : expected) {
+            assertEquals(key, it.next().getValueMap().get(PROPNAME, new String[0])[0]);
+        }
+        assertFalse(it.hasNext());
+    }
+
+    private static Collection<Resource> toResourceList(String... keys) {
+        Collection<Resource> result = new ArrayList<>();
+        for (String key : keys) {
+            ValueMap m = mock(ValueMap.class);
+            when(m.get(eq(PROPNAME), any(Object.class))).thenReturn(new String[] { key });
+            Resource r = mock(Resource.class);
+            when(r.getValueMap()).thenReturn(m);
+            result.add(r);
+        }
+        return result;
     }
 }
